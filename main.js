de373c522fea7a45e59360b3d099f1b7852cda01
Vincent Asante <vincenthokie@gmail.com>
2018-06-26 12:03:26 +0300

    create initial project structure

---
+++ main.js
@@ -0,0 +1,5 @@
+let hello = () => {
+    console.log("Im awesome");
+}
+
+hello()
---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const parentDir = path.join(__dirname, '../');
+
+module.exports = {
+  // define js entry point i.e. what will be loaded by our base template directly
+  entry: [
+    path.join(parentDir, 'main.js'),
+  ],
+  module: {
+    rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader', // turn all .js and jsx files that are not in node_modules into the more compatible es5
+    }, {
+      test: /\.(css|scss)$/,
+      loaders: [
+        'style-loader', // creates style nodes from JS strings
+        'css-loader', // translates CSS into CommonJS
+        'sass-loader', // compiles Sass to CSS
+      ],
+    }],
+  },
+  // defines where all our js will be bundled for use by react
+  output: {
+    path: path.join(parentDir, 'build'),
+    filename: 'main.bundle.js',
+  },
+  devServer: {
+    contentBase: parentDir,
+    historyApiFallback: true,
+  },
+};
