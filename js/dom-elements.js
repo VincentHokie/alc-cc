c1844b788f539b768b9aeaa6628969cdf5d7206f
Vincent Asante <vincenthokie@gmail.com>
2018-06-29 15:40:33 +0300

    add the basic functionality with single and double conversion as well as exchange rate history

---
+++ js/dom-elements.js
@@ -0,0 +1,26 @@
+
+export const singleConversion = document.getElementById("single_conversion");
+export const currencyFrom = document.getElementById("currency_from");
+export const currencyTo = document.getElementById("currency_to");
+
+export const doubleConversion = document.getElementById("double_conversion");
+export const doubleCurrencyFrom = document.getElementById("double_currency_from");
+export const doubleCurrencyToOne = document.getElementById("double_currency_to_one");
+export const doubleCurrencyToTwo = document.getElementById("double_currency_to_two");
+
+export const historyCurrencyFromOne = document.getElementById("history_currency_from_one");
+export const historyCurrencyFromTwo = document.getElementById("history_currency_from_two");
+export const historyCurrencyToOne = document.getElementById("history_currency_to_one");
+export const historyCurrencyToTwo = document.getElementById("history_currency_to_two");
+
+export const singleMenu = document.getElementById("single");
+export const doubleMenu = document.getElementById("double");
+export const historyMenu = document.getElementById("history");
+export const usageMenu = document.getElementById("usage");
+
+export const singleContainer = document.getElementById("single_container");
+export const doubleContainer = document.getElementById("double_container");
+export const historyContainer = document.getElementById("history_container");
+export const spaContainers = document.getElementsByClassName("something_container");
+
+export const dateStart = document.getElementById("dateStart");
