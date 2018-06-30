559a3face4b45115a0625fbce6cb90c388f793d3
Vincent Asante <vincenthokie@gmail.com>
2018-06-30 23:25:37 +0300

    fix sw redundancy issue

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ theDom.historyMenu.addEventListener("click", () => {
 
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/js/sw.js')
+    navigator.serviceWorker.register('/sw.js')
     .then((reg) => {
         // registration worked
         console.log('Registration succeeded. Scope is ' + reg.scope);
