c67dc8c8d9de68128b9f6215d55b8c5ae6444be2
Vincent Asante <vincenthokie@gmail.com>
2018-06-30 23:45:37 +0300

    use a string for the sw version instead of a variable

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const SW_VERSION = "v1";
 
   self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches.open(SW_VERSION.toString()).then((cache) => {
+      caches.open("v1").then((cache) => {
         return cache.addAll([
           '/',
           '/index.html',
@@ -29,7 +29,7 @@ const SW_VERSION = "v1";
       caches.match(event.request).then((resp) => {
         return resp || fetch(event.request).then((response) => {
           let responseClone = response.clone();
-          caches.open(SW_VERSION.toString()).then((cache) => {
+          caches.open("v1").then((cache) => {
             cache.put(event.request, responseClone);
           });
   
