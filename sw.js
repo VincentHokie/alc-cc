8365ec31c4361af71f43ef2a3359804a38f2b0df
Vincent Asante <vincenthokie@gmail.com>
2018-07-01 23:33:37 +0300

    - allow deletion of old caches when a new one is available - add iziToast package for notifications - add user notifications when there are errors and   information when were doing conversions offline - cache new css asset

--- sw.js
+++ sw.js
@@ -4,11 +4,11 @@
 */
 
 
-const SW_VERSION = "v1";
+const expectedCaches = ['static-v2'];
 
   self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches.open("v1").then((cache) => {
+      caches.open("static-v2").then((cache) => {
         return cache.addAll([
           '/',
           '/index.html',
@@ -16,6 +16,7 @@ const SW_VERSION = "v1";
           '/static/images/coin.jpg',
           '/static/images/img-loading.gif',
           '/static/js/Chart.bundle.min',
+          '/static/css/iziToast.min.css',
         ]);
       }).catch((error) => {
         console.log("error", error)
@@ -23,14 +24,26 @@ const SW_VERSION = "v1";
     );
   });
 
-
+  self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(keys => Promise.all(
+        keys.map(key => {
+          if (!expectedCaches.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )).then(() => {
+        console.log('V2 now ready to handle fetches!');
+      })
+    );
+  });
 
   self.addEventListener('fetch', (event) => {
     event.respondWith(
       caches.match(event.request).then((resp) => {
         return resp || fetch(event.request).then((response) => {
           let responseClone = response.clone();
-          caches.open("v1").then((cache) => {
+          caches.open("static-v2").then((cache) => {
             cache.put(event.request, responseClone);
           });
   
